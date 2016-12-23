62144c8b40dd56a49c1677e61f494d5fdefd9d39
JeffGoderie <jeffrey.goderie@gmail.com>
2016-12-23 16:55:29 +0100

    Merge things together

--- Assets/DrawGraph.cs
+++ Assets/DrawGraph.cs
@@ -4,28 +4,47 @@ using UnityEngine;
 
 using System.Text;
 using System.IO;
+using System.Linq;
+
+using UnityEngine.UI.Extensions;
 
 public class DrawGraph : MonoBehaviour {
 
-
+/**
 	[SerializeField]
 	private GameObject lineRender; 
 	[SerializeField]
 	private Camera m_Camera;
-
+**/
 	private Vector3[] m_Positions;
 
 	private GameObject[] m_LineRenderers;
 
 	private float m_PointSpacingX = 10;
 
+	private Vector2[] points;
+
+	private int currentIndex;
+
+	public UILineRenderer indicator;
+
+	List<float> angles;
+
+	float height;
+	float width;
+	float hScale;
+	float wScale;
+
 	// Use this for initialization
 	void Start () {
-		string fileName = "D:\\\\workspace\\MotorAR\\points.txt";
-		List<float> angles = new List<float>(); // gets all lines into separate strings
+		string fileName = ".\\points.txt";
+		angles = new List<float>(); // gets all lines into separate strings
 		//Debug.Log(angles);
 		//Debug.Log ("blahblahblah " + angles.Length);
 		float currentPosX = 0;
+
+		Debug.Log(transform.parent.GetComponent<RectTransform>().rect.width);
+		Debug.Log(transform.parent.GetComponent<RectTransform>().rect.height);
 
 		string line; 
 		StreamReader theReader = new StreamReader(fileName, Encoding.Default);
@@ -39,6 +58,30 @@ public class DrawGraph : MonoBehaviour {
 			} while(line != null);
 		}
 
+		points = new Vector2[angles.Count];
+
+		float offset = 25.0f;
+
+		height = transform.parent.GetComponent<RectTransform>().rect.height - 2*offset;
+		width = transform.parent.GetComponent<RectTransform>().rect.width - 2*offset;
+		hScale = height/angles.Max();
+		wScale = width/((angles.Count-1) * m_PointSpacingX);
+
+		Debug.Log(hScale);
+
+		for (int i = 0; i < angles.Count; i++) {
+			Debug.Log(i*m_PointSpacingX*wScale - width/2);
+			points[i] = new Vector2(i*m_PointSpacingX*wScale - width/2, angles[i]*hScale - height/2);
+		}
+
+		GetComponent<UILineRenderer>().Points = points;
+
+		currentIndex = 0;
+
+		setIndicatorPosition(currentIndex*m_PointSpacingX*wScale - width/2);
+
+
+/**
 		for (int i = 0; i < angles.Count - 1; i++) {
 			m_LineRenderers = new GameObject[angles.Count];
 			m_LineRenderers[i] = (GameObject) Instantiate (lineRender, Vector3.zero, Quaternion.identity);
@@ -61,8 +104,34 @@ public class DrawGraph : MonoBehaviour {
 		
 	// Update is called once per frame
 	void Update () {
-		
+		**/
 	}
-		
+
+	private void setIndicatorPosition(float position){
+		Vector2[] points = new Vector2[2];
+
+		points[0] = new Vector2(position,-1000f);
+		points[1] = new Vector2(position,1000f);
+
+		indicator.Points = points;
+	}
+
+	void Update (){
+
+        if (Input.GetKeyDown("left")){
+        	if(currentIndex > 0){
+        		currentIndex -= 1;
+				setIndicatorPosition(currentIndex*m_PointSpacingX*wScale - width/2);
+        	}
+        }
+        
+        if (Input.GetKeyDown("right")){
+            if(currentIndex < angles.Count()-1){
+        		currentIndex += 1;
+				setIndicatorPosition(currentIndex*m_PointSpacingX*wScale - width/2);
+        	}
+    	}
+
+	}
 
 }
---
+++ Assets/Slider/SliderScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SliderScript : MonoBehaviour {
+
+	public bool isFloatSlider;
+
+	// Use this for initialization
+	void Start () {
+		if(isFloatSlider){
+			transform.Find("Variable Value Text").GetComponent<Text>().text = GetComponent<Slider>().value.ToString(".##");
+		}
+		else{
+			transform.Find("Variable Value Text").GetComponent<Text>().text = GetComponent<Slider>().value.ToString();
+		}
+		GetComponent<Slider>().onValueChanged.AddListener (delegate {ValueChanged();});
+	}
+	
+	private void ValueChanged() {
+		if(isFloatSlider){
+			transform.Find("Variable Value Text").GetComponent<Text>().text = GetComponent<Slider>().value.ToString(".##");
+		}
+		else{
+			transform.Find("Variable Value Text").GetComponent<Text>().text = GetComponent<Slider>().value.ToString();
+		}
+	}
+}
